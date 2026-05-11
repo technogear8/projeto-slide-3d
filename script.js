a688cf88e1bd0b5484b00563a938b2f578c225fa
royer <arthur.royer.nogueira@escola.pr.gov.br>
2026-05-11 11:39:06 -0300

    ..

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  const botoes = document.querySelectorAll(".botao");
  for (let i =0; i < botoes.length; i++){
     botoes[i].onclick = function () {
-        botoes[i].classList(".Active")
+        botoes[i].classList.add(".Active")
     }
  console.log(botoes);
  }
